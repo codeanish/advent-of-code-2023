d7cd4f5f2498db5a44ac926c551797be0be5e5fd
Anish <anish@codeanish.com>
2023-12-10 15:31:01 -0800

    Day 10 complete

--- day10/day10/main.py
+++ day10/day10/main.py
@@ -1,7 +1,11 @@
-from typing import List
+from typing import List, Tuple
 
 from puzzle_map import PuzzleMap
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.style.use('_mpl-gallery')
 
 def read_text_file_to_lines(filename: str) -> List[str]:
     my_file = open(filename, "r")
@@ -11,12 +15,72 @@ def read_text_file_to_lines(filename: str) -> List[str]:
     return data_to_list
 
 
-
-
 if __name__ == "__main__":
-    data = read_text_file_to_lines("day10\example_1.txt")
+    data = read_text_file_to_lines("day10\input.txt")
     puzzle_map = PuzzleMap(data)
     # print(puzzle_map.get_adjoining_pipes(77,34))
+    # starting_coordinates = puzzle_map.get_starting_coordinates()
     loop = puzzle_map.get_loop()
-    print(loop)
+    # find a point on an edge of the loop, then traverse the loop, 
+    # points to one side of the line are not included, points to the other side of the line are included
+    contained_points = 0
+    # Replace all points not on loop with '.'
+    for idy, line in enumerate(puzzle_map.matrix):
+        for idx, point in enumerate(line):
+            if (idx, idy) not in loop:
+                puzzle_map.matrix[idy][idx] = '.'
+    for idy, line in enumerate(puzzle_map.matrix):
+        for idx, point in enumerate(line):
+            if puzzle_map.odd_pipe_crossings((idx, idy)):
+                # print((idx,idy))
+                if (idx,idy) not in loop:        
+                    contained_points += 1
+    
+    print(contained_points)
+    # print(loop)
+
+    # northernmost_points = []
+    # for point in loop:
+    #     if len(northernmost_points) == 0:
+    #         northernmost_points.append(point)
+    #         continue
+    #     if point[1] < northernmost_points[0][1]:
+    #         northernmost_points = [point]
+    #     if point[1] == northernmost_points[0][1]:
+    #         northernmost_points.append(point)
+    # starting_point = northernmost_points[0]
+    # for point in northernmost_points:
+    #     if point[0] < starting_point[0]:
+    #         starting_point = point
+    # # print(northernmost_points)
+    # print(starting_point)
+    # adjoining_points = puzzle_map.get_adjoining_pipes(starting_point[0], starting_point[1])
+    # # next point = point where x(new) = x+1
+    # print(adjoining_points)
+    # current_point = None
+    # if adjoining_points[0][0] > starting_point[0]:
+    #     current_point = adjoining_points[0]
+    # else:
+    #     current_point = adjoining_points[1]
+    # previous_point = starting_point
+    # previous_points = [starting_point]
+    # while current_point[0] != starting_point[0] or current_point[1] != starting_point[1]:
+    #     print(current_point)
+    #     next_point = puzzle_map.get_next_point(current_point, previous_point)
+    #     previous_point = current_point
+    #     current_point = next_point
+        
+
+    # loop.append(starting_coordinates)
+    # x = []
+    # y = []
+    # for item in loop:
+    #     x.append(item[0])
+    #     y.append(item[1])
+    # x_np = np.array(x)
+    # y_np = np.array(y)
+    # fig, ax = plt.subplots()
+    # ax.plot(x_np, y_np, linewidth=2.0)
+    # plt.show()
+    # print(loop)
     
--- day10/day10/puzzle_map.py
+++ day10/day10/puzzle_map.py
@@ -39,26 +39,153 @@ class PuzzleMap:
                 current_pipe = adjoining_pipes[1]
             loop.append(current_pipe)
         return loop
-        
 
-    def get_length_of_loop(self) -> int:
-        starting_coordinates = self.get_starting_coordinates()
-        adjoining_pipes = self.get_adjoining_pipes(starting_coordinates[0], starting_coordinates[1])
-        s_type = self.what_is_s(adjoining_pipes)
-        self.matrix[starting_coordinates[1]][starting_coordinates[0]] = s_type
-        previous_pipe = starting_coordinates
-        current_pipe = adjoining_pipes[0]
-        length = 1
-        while current_pipe[0] != starting_coordinates[0] or current_pipe[1] != starting_coordinates[1]:
-            adjoining_pipes = self.get_adjoining_pipes(current_pipe[0], current_pipe[1])
-            if adjoining_pipes[0][0] != previous_pipe[0] or adjoining_pipes[0][1] != previous_pipe[1]:
-                previous_pipe = current_pipe
-                current_pipe = adjoining_pipes[0]
-            else:
-                previous_pipe = current_pipe
-                current_pipe = adjoining_pipes[1]
-            length += 1
-        return length
+    def get_next_point(self, current_point: Tuple[int,int], previous_point: Tuple[int,int]):
+        adjoining_pipes = self.get_adjoining_pipes(current_point[0], current_point[1])
+        if adjoining_pipes[0][0] != previous_point[0] or adjoining_pipes[0][1] != previous_point[1]:
+            return adjoining_pipes[0]
+        else:
+            return adjoining_pipes[1]
+
+
+    def odd_pipe_crossings(self, point: Tuple[int,int]):
+        nodes_left, nodes_right, nodes_up, nodes_down = [], [], [], []
+
+        for i in range(0, point[0]):
+            nodes_left.append((i, point[1]))
+        for i in range(point[0] + 1, len(self.matrix[0])):
+            nodes_right.append((i, point[1]))
+        for i in range(0, point[1]):
+            nodes_up.append((point[0], i))
+        for i in range(point[1] + 1, len(self.matrix)):
+            nodes_down.append((point[0], i))
+
+        left_crossings = 0
+        last_direction_up = None
+        for node in nodes_left:
+            value = self.get_value_at_coordinates(node[0], node[1])
+            if value == '|':
+                left_crossings += 1
+            elif value == 'F' or value == '7':
+                if last_direction_up:
+                    left_crossings += 1
+                    last_direction_up = None
+                else:
+                    if last_direction_up is None:
+                        last_direction_up = False
+                    else: 
+                        last_direction_up = None
+            elif value == 'L' or value == 'J':
+                if last_direction_up == False:
+                    left_crossings += 1
+                    last_direction_up = None
+                else:
+                    if last_direction_up is None:
+                        last_direction_up = True
+                    else: 
+                        last_direction_up = None
+        
+        right_crossings = 0
+        last_direction_up = None
+        for node in nodes_right:
+            value = self.get_value_at_coordinates(node[0], node[1])
+            if value == '|':
+                right_crossings += 1
+            elif value == 'F' or value == '7':
+                if last_direction_up:
+                    right_crossings += 1
+                    last_direction_up = None
+                else:
+                    if last_direction_up is None:
+                        last_direction_up = False
+                    else: 
+                        last_direction_up = None
+            elif value == 'L' or value == 'J':
+                if last_direction_up == False:
+                    right_crossings += 1
+                    last_direction_up = None
+                else:
+                    if last_direction_up is None:
+                        last_direction_up = True
+                    else: 
+                        last_direction_up = None
+        
+        last_direction_left = None
+        up_crossings = 0
+        for node in nodes_up:
+            value = self.get_value_at_coordinates(node[0],node[1])
+            if value == '-':
+                up_crossings += 1
+            elif value == 'L' or value == 'F':
+                if last_direction_left:
+                    up_crossings += 1
+                    last_direction_left = None
+                else:
+                    if last_direction_left is None:
+                        last_direction_left = False
+                    else:
+                        last_direction_left = None
+            elif value == 'J' or value == '7':
+                if last_direction_left == False:
+                    up_crossings += 1
+                    last_direction_left = None
+                else:
+                    if last_direction_left is None:
+                        last_direction_left = True
+                    else:
+                        last_direction_left = None
+        
+        last_direction_left = None
+        down_crossings = 0
+        for node in nodes_down:
+            value = self.get_value_at_coordinates(node[0],node[1])
+            if value == '-':
+                down_crossings += 1
+            elif value == 'L' or value == 'F':
+                if last_direction_left:
+                    down_crossings += 1
+                    last_direction_left = None
+                else:
+                    if last_direction_left is None:
+                        last_direction_left = False
+                    else:
+                        last_direction_left = None
+            elif value == 'J' or value == '7':
+                if last_direction_left == False:
+                    down_crossings += 1
+                    last_direction_left = None
+                else:
+                    if last_direction_left is None:
+                        last_direction_left = True
+                    else:
+                        last_direction_left = None
+
+
+        if left_crossings % 2 == 1 and right_crossings % 2 == 1 and up_crossings % 2 == 1 and down_crossings % 2 == 1:
+            return True
+        return False
+            
+
+            
+
+    # def get_length_of_loop(self) -> int:
+    #     starting_coordinates = self.get_starting_coordinates()
+    #     adjoining_pipes = self.get_adjoining_pipes(starting_coordinates[0], starting_coordinates[1])
+    #     s_type = self.what_is_s(adjoining_pipes)
+    #     self.matrix[starting_coordinates[1]][starting_coordinates[0]] = s_type
+    #     previous_pipe = starting_coordinates
+    #     current_pipe = adjoining_pipes[0]
+    #     length = 1
+    #     while current_pipe[0] != starting_coordinates[0] or current_pipe[1] != starting_coordinates[1]:
+    #         adjoining_pipes = self.get_adjoining_pipes(current_pipe[0], current_pipe[1])
+    #         if adjoining_pipes[0][0] != previous_pipe[0] or adjoining_pipes[0][1] != previous_pipe[1]:
+    #             previous_pipe = current_pipe
+    #             current_pipe = adjoining_pipes[0]
+    #         else:
+    #             previous_pipe = current_pipe
+    #             current_pipe = adjoining_pipes[1]
+    #         length += 1
+    #     return length
                 
     def what_is_s(self, adjoining_pipes: List[Tuple[int,int]]):
         first_pipe = adjoining_pipes[0]
@@ -105,14 +232,22 @@ class PuzzleMap:
                 adjoining_pipes.append((x, y+1))
         return adjoining_pipes
     
+def read_text_file_to_lines(filename: str) -> List[str]:
+    my_file = open(filename, "r")
+    data = my_file.read()
+    data_to_list = data.splitlines()
+    my_file.close()
+    return data_to_list
 
 if __name__ == "__main__":
-    data = [".....",".S-7.",".|.|.",".L-J.","....."]
+    data = read_text_file_to_lines("day10\example_part_2.txt")
+    # data = [".....",".S-7.",".|.|.",".L-J.","....."]
     # data = ["..F7.",".FJ|.","SJ.L7","|F--J","LJ..."]
     puzzle_map = PuzzleMap(data)
+    puzzle_map.odd_pipe_crossings((14,3))
     # adjoining_pipes = puzzle_map.get_adjoining_pipes(1,1)
     # s_type = puzzle_map.what_is_s(adjoining_pipes)
     # print(adjoining_pipes)
     # print(s_type)
-    length = puzzle_map.get_length_of_loop()
-    print(length // 2)
+    # length = puzzle_map.get_length_of_loop()
+    # print(length // 2)
